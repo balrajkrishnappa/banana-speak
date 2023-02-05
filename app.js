d4693cf6400d36a248bbe555071cd024b5f0c18b
balrajbossu <balrajbhagyed@gmail.com>
2023-02-05 22:34:31 +0530

    Added Styles,header fromGIT

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var translateButton = document.querySelector("#btn-translate");
 
 var URL = "https://api.funtranslations.com/translate/minion.json";
 
+function errorHandler(error) {
+  console.log("error occured", error);
+  alert("something wrong with the server | try agin after some time " + error);
+}
 function finalURL(text) {
   var finalURLString = URL + "?text=" + text;
   return finalURLString;
@@ -12,7 +16,8 @@ function fetchfunction() {
   var textinput = txtinput.value;
   fetch(finalURL(textinput))
     .then((request) => request.json())
-    .then((json) => (translatedOutput.innerText = json.contents.translated));
+    .then((json) => (translatedOutput.innerText = json.contents.translated))
+    .catch(errorHandler);
 }
 
 translateButton.addEventListener("click", fetchfunction);
