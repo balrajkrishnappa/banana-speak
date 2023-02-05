418e4eefc90b8bc05e738054350a91729cb03a63
balrajbossu <balrajbhagyed@gmail.com>
2023-02-05 21:41:36 +0530

    Done with coding part fromGIT

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("It works");
+var txtinput = document.querySelector("#txt-area");
+var translatedOutput = document.querySelector("#translated-area");
+var translateButton = document.querySelector("#btn-translate");
+
+var URL = "https://api.funtranslations.com/translate/minion.json";
+
+function finalURL(text) {
+  var finalURLString = URL + "?text=" + text;
+  return finalURLString;
+}
+function fetchfunction() {
+  var textinput = txtinput.value;
+  fetch(finalURL(textinput))
+    .then((request) => request.json())
+    .then((json) => (translatedOutput.innerText = json.contents.translated));
+}
+
+translateButton.addEventListener("click", fetchfunction);
